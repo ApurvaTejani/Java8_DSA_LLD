78b5bfd0bbbd7c797725526039ab77b6d940c0f9
Apurva Tejani <aputejacc@gmail.com>
2024-02-26 21:41:05 +0530

    Initial Commit

---
+++ MethodTutorial/MainChallenge.java
@@ -0,0 +1,15 @@
+public class  MainChallenge {
+    public static void main(String[] args) {
+       int age =20;
+       methodDoes(age);
+    }
+
+    public static void methodDoes(int age) {
+        if(age<21)
+        {
+            return;
+        }
+        age+=100;
+        System.out.println(age);
+    }
+}
