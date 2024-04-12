6ec06c7528602640ab870a759634a39e04b0b986
ApurvaTejani <aputejacc@gmail.com>
2024-04-12 15:22:47 +0530

    Added few programs initial commit

---
+++ CodingBat/WarmUp_2.0/CountXX.java
@@ -0,0 +1,29 @@
+
+// Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+// countXX("abcxx") → 1 // 
+// countXX("xxx") → 2 
+// countXX("xxxx") → 3
+public class CountXX {
+    public static void main(String[] args) {
+        System.out.println(countXX(""));
+    }
+
+    public static int countXX(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+        String[] newStr = new String[str.length() - 1];
+        int count = 0;
+        if (str.contains("xx")) {
+            for (int i = 0; i < newStr.length; i++) {
+                newStr[i] = str.substring(i, (i + 2));
+                if (newStr[i].equals("xx")) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+}
---
+++ CodingBat/WarmUp_2.0/has271.java
@@ -0,0 +1,32 @@
+
+// Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+
+// has271([1, 2, 7, 1]) → true
+// has271([1, 2, 8, 1]) → false
+// has271([2, 7, 1]) → true
+
+import java.util.Scanner;
+
+public class has271 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int arr[] = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(has271Method(arr));
+    }
+
+    public static boolean has271Method(int arr[]) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            // if ((arr[i] + 5 == arr[i + 1]) && ((arr[i] - 1 == arr[i + 2]) || (arr[i] - 2
+            // == arr[i + 2]))) {
+            if ((arr[i] + 5 == arr[i + 1]) && (Math.abs(arr[i + 2] - (arr[i] - 1)) <= 2)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
