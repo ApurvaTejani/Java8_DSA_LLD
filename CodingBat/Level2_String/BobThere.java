2ea8b8d43206763a139c85fa6d67d425eaeb52f6
ApurvaTejani <aputejacc@gmail.com>
2024-04-14 00:57:48 +0530

    Solved 2 more problems

---
+++ CodingBat/Level2_String/BobThere.java
@@ -0,0 +1,22 @@
+package CodingBat.Level2_String;
+
+// Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+// bobThere("abcbob") → true
+// bobThere("b9b") → true
+// bobThere("bac") → false
+public class BobThere {
+    public static void main(String[] args) {
+        System.out.println(bobThere("bac"));
+    }
+
+    public static boolean bobThere(String str) {
+        char[] letters = str.toCharArray();
+        for (int i = 0; i < letters.length - 2; i++) {
+            if (letters[i] == 'b' && letters[i + 2] == 'b') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ CodingBat/Level2_String/RepeatEnd.java
@@ -0,0 +1,24 @@
+package CodingBat.Level2_String;
+
+// Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+// repeatEnd("Hello", 3) → "llollollo"  // 2
+// repeatEnd("Hello", 2) → "lolo"// 3
+// repeatEnd("Hello", 1) → "o"//4
+public class RepeatEnd {
+    public static void main(String[] args) {
+        System.out.println(repeatEnd("Hello", 3));
+    }
+
+    public static String repeatEnd(String str, int c) {
+        String str1 = str.substring(str.length() - c);
+        String[] strArr = new String[c];
+        String newStr = "";
+        for (int i = 0; i < c; i++) {
+            strArr[i] = str1;
+            newStr = newStr.concat(strArr[i]);
+        }
+
+        return newStr;
+    }
+}
