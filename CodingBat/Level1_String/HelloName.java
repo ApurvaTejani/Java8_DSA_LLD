5e93134a6463d10ff95b7616b23d4f0557bb6a9f
ApurvaTejani <aputejacc@gmail.com>
2024-04-13 22:31:05 +0530

    Intial Commit Started String level 1 and 2

---
+++ CodingBat/Level1_String/HelloName.java
@@ -0,0 +1,23 @@
+package CodingBat.Level1_String;
+
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+// helloName("Bob") → "Hello Bob!"
+// helloName("Alice") → "Hello Alice!"
+// helloName("X") → "Hello X!
+public class HelloName {
+    public static void main(String[] args) {
+
+        System.out.println(helloName("Bob"));
+    }
+
+    public static String helloName(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+        String str1 = "Hello ";
+        str1 = str1.concat(str);
+        str1 = str1 + "!";
+        return str1;
+    }
+}
---
+++ CodingBat/Level1_String/MakeOutWord.java
@@ -0,0 +1,20 @@
+package CodingBat.Level1_String;
+
+// Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+// makeOutWord("<<>>", "Yay") → "<<Yay>>"
+// makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+// makeOutWord("[[]]", "word") → "[[word]]"
+public class MakeOutWord {
+    public static void main(String[] args) {
+        System.out.println(makeOutWord("[[]]", "word"));
+    }
+
+    public static String makeOutWord(String estr, String str) {
+        String estr1 = estr.substring(0, 2);
+        String estr2 = estr.substring(2, 4);
+        str = (estr1.concat(str)).concat(estr2);
+        return str;
+    }
+
+}
