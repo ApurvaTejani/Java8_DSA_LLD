36ef6172ce8e4617a7a197788752807eafc248b9
ApurvaTejani <aputejacc@gmail.com>
2024-04-14 18:49:52 +0530

    Added the InterThreadProgramming

---
+++ AbdulBariJava/MultiThreading/InterThreadSync/Consumer.java
@@ -0,0 +1,15 @@
+package AbdulBariJava.MultiThreading.InterThreadSync;
+
+public class Consumer extends Thread {
+    MyData d;
+
+    public Consumer(MyData d) {
+        this.d = d;
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("Conusmer" + d.getData());
+        }
+    }
+}
---
+++ AbdulBariJava/MultiThreading/InterThreadSync/Main.java
@@ -0,0 +1,11 @@
+package AbdulBariJava.MultiThreading.InterThreadSync;
+
+public class Main {
+    public static void main(String[] args) {
+        MyData d = new MyData();
+        Producer t1 = new Producer(d);
+        Consumer t2 = new Consumer(d);
+        t1.start();
+        t2.start();
+    }
+}
