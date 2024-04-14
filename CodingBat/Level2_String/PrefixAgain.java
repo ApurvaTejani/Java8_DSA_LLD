fd51b0b6e1666a71cda41602d8c846ecfc34a4a3
ApurvaTejani <aputejacc@gmail.com>
2024-04-14 12:39:04 +0530

    Initial Commit

---
+++ CodingBat/Level2_String/PrefixAgain.java
@@ -0,0 +1,21 @@
+package CodingBat.Level2_String;
+
+// Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length().
+
+// prefixAgain("abXYabc", 1) → true
+// prefixAgain("abXYabc", 2) → true
+// prefixAgain("abXYabc", 3) → false
+class PrefixAgain {
+    public static void main(String[] args) {
+        System.out.println(prefixAgain("abXYabc", 3));
+
+    }
+
+    public static boolean prefixAgain(String str, int c) {
+        String subStr = str.substring(0, c);
+        String newStr = str.substring(c);
+        boolean flag = newStr.contains(subStr);
+        return flag;
+    }
+
+}
