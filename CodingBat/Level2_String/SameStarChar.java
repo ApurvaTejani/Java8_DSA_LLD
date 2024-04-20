d69d80a108e5e6cced39c3c3aede7eba2df39ade
ApurvaTejani <aputejacc@gmail.com>
2024-04-20 13:20:47 +0530

    Solved logic issue for SameStarChar program

--- CodingBat/Level2_String/SameStarChar.java
+++ CodingBat/Level2_String/SameStarChar.java
@@ -1,5 +1,6 @@
 package CodingBat.Level2_String;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,27 +9,44 @@ import java.util.Set;
 // sameStarChar("xy*yzz") → true
 // sameStarChar("xy*zzz") → false
 // sameStarChar("*xa*az") → true
+
+// =================================Logic issues resolved===========================
 public class SameStarChar {
     public static void main(String[] args) {
-        System.out.println(sameStarChar("*12*2*2"));
+        System.out.println(sameStarChar("**"));
+
     }
 
     public static boolean sameStarChar(String str) {
-        boolean[] flag = new boolean[str.length()];
+        ArrayList list = new ArrayList<>();
+        char[] letters = str.toCharArray();
+        if (!str.contains("*"))
+            return true;
+        for (int z = 0; z < str.length(); z++) {
+            if (letters[z] == '*' && (z != 0 && z != str.length() - 1))
+                list.add(1);
+        }
+        boolean flag[] = new boolean[list.size()];
         int j = 0;
-        if (str.contains("*")) {
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '*' && (i != 0 && i != str.length() - 1)) {
-                    if (str.charAt(i - 1) == str.charAt(i + 1))
-                        flag[j] = true;
-                    j++;
-                }
-            }
-            Set<Boolean> s = new HashSet<>(Arrays.asList(flag));
-            if (s.size() == 1) {
-                return true;
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*' && (i != 0 && i != str.length() - 1)) {
+                if (str.charAt(i - 1) == str.charAt(i + 1))
+                    flag[j] = true;
+                j++;
             }
         }
+
+        for (boolean f : flag) {
+            if (f == true)
+                count++;
+        }
+
+        if (count == list.size())
+            return true;
+
         return false;
     }
+
 }
