742b2f760b7049f48bb08948e0ffd5d78e5a1db1
ApurvaTejani <aputejacc@gmail.com>
2024-04-27 12:40:36 +0530

    Intial Commit

--- CodingBat/Level2_Arrays/Sum13.java
+++ CodingBat/Level2_Arrays/Sum13.java
@@ -1,5 +1,10 @@
 package CodingBat.Level2_Arrays;
 
+// Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+// sum13([1, 2, 2, 1]) → 6
+// sum13([1, 1]) → 2
+// sum13([1, 2, 2, 1, 13]) → 6
 public class Sum13 {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 13, 1 };
