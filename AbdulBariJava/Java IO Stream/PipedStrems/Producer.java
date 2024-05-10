999fb9fa5b8d4ec4d9ad013c4defc76cf2248aa0
ApurvaTejani <aputejacc@gmail.com>
2024-05-10 20:35:21 +0530

    Piped Stream Concept Explained

---
+++ AbdulBariJava/Java IO Stream/PipedStrems/Producer.java
@@ -0,0 +1,28 @@
+import java.io.OutputStream;
+
+public class Producer extends Thread {
+    OutputStream os;
+
+    public Producer(OutputStream o) {
+        os = o;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int count = 0;
+            while (true) {
+                os.write(count);
+                os.flush();
+                // Thread.sleep(100);
+                System.out.println("Producer " + count);
+                System.out.flush();
+
+                count++;
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+}
