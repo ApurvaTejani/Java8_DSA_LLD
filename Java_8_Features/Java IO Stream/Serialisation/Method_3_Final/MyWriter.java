09927d64c66ff7632fd91f1f17fa150248101fd2
ApurvaTejani <aputejacc@gmail.com>
2024-05-16 12:44:36 +0530

    Concept of Object i/o Stream

---
+++ Java_8_Features/Java IO Stream/Serialisation/Method_3_Final/MyWriter.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class MyWriter {
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fos = new FileOutputStream(
+                "C:/Users/apurv/OneDrive/AllCodes/Core_java/Udemy+Scaler/src/Java_8_Features/Java IO Stream/Serialisation/Method_3_Final/student3.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        Student s = new Student(20, "Apu", "EXTC");
+        oos.writeObject(s);
+
+    }
+}
---
+++ Java_8_Features/Java IO Stream/Serialisation/Method_3_Final/Student.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    int rollno;
+    String name;
+    String dept;
+
+    static String schoolName = "CA";
+
+    public Student() {
+    }
+
+    public Student(int rollno, String name, String dept) {
+        this.rollno = rollno;
+        this.name = name;
+        this.dept = dept;
+        schoolName = "Bharti";
+    }
+
+    public String toString() {
+        return rollno + " " + name + " " + dept + " " + schoolName;
+    }
+}
