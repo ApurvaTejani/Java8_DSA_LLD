bb2711e8cef09e464d0026b3ecf306a6740f0235
ApurvaTejani <aputejacc@gmail.com>
2024-05-17 23:24:36 +0530

    Added a feature of showing client and server IP address when connected

---
+++ Java_8_Features/NetworkProgramming/SingleThread/Client.java
@@ -0,0 +1,24 @@
+package Java_8_Features.NetworkProgramming;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket stk = new Socket("172.20.58.202", 20);
+        // Display server's IP address
+        InetAddress serverAddress = stk.getInetAddress();
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader keyboard = new BufferedReader(isr);
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+        String msg;
+        do {
+            msg = keyboard.readLine();
+            ps.println(msg);
+            msg = br.readLine();
+            System.out.println("From Server " + serverAddress + " " + msg);
+        } while (!msg.equals("dne"));
+    }
+}
