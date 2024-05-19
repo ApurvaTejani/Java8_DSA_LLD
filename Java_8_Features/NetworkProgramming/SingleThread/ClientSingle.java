2c3e62f78bdf74acdc6aa0a23941d84a7250470b
ApurvaTejani <aputejacc@gmail.com>
2024-05-19 10:46:46 +0530

    Minor Changes in Single Thread TCP communication

--- Java_8_Features/NetworkProgramming/SingleThread/ClientSingle.java
+++ Java_8_Features/NetworkProgramming/SingleThread/ClientSingle.java
@@ -5,20 +5,23 @@ import java.net.*;
 
 public class ClientSingle {
     public static void main(String[] args) throws Exception {
-        Socket stk = new Socket("172.20.58.202", 20);
-        // Display server's IP address
-        InetAddress serverAddress = stk.getInetAddress();
+        try(Socket stk = new Socket("localhost", 2000)) {
 
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader keyboard = new BufferedReader(isr);
-        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
-        PrintStream ps = new PrintStream(stk.getOutputStream());
-        String msg;
-        do {
-            msg = keyboard.readLine();
-            ps.println(msg);
-            msg = br.readLine();
-            System.out.println("From Server " + serverAddress + " " + msg);
-        } while (!msg.equals("dne"));
+            // Display server's IP address
+            InetAddress serverAddress = stk.getInetAddress();
+
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader keyboard = new BufferedReader(isr);
+            BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+            PrintStream ps = new PrintStream(stk.getOutputStream());
+            String msg;
+            do {
+                msg = keyboard.readLine();
+                ps.println(msg);
+                msg = br.readLine();
+                System.out.println("From Server " + serverAddress + " " + msg);
+            } while (!msg.equals("dne"));
+        }
+
     }
 }
