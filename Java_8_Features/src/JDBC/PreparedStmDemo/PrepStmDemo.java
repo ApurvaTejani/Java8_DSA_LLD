fb048117c34d198f94045724a197f3cf05dd75cd
ApurvaTejani <aputejacc@gmail.com>
2024-05-21 10:36:59 +0530

    Concept of prepared Statements

---
+++ Java_8_Features/src/JDBC/PreparedStmDemo/PrepStmDemo.java
@@ -0,0 +1,23 @@
+package JDBC.PreparedStmDemo;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class PrepStmDemo {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\SQLLite\\univ.db");
+        PreparedStatement pstm = con.prepareStatement("SELECT *FROM  Students where deptno=?");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the dept number : ");
+        int deptno = sc.nextInt();
+        pstm.setInt(1, deptno);
+        ResultSet rs = pstm.executeQuery();
+        while (rs.next()) {
+            System.out.print(rs.getInt(1) + " ");
+            System.out.print(rs.getString(2) + " ");
+            System.out.print(rs.getString(3) + " ");
+            System.out.println(rs.getInt(4));
+        }
+    }
+}
