2e8adc5bbfed1d05952aeae3e0af3f9b70edd976
ApurvaTejani <aputejacc@gmail.com>
2024-05-23 17:05:08 +0530

    Added before java 8 way to do so

--- Java_8_Features/src/Lambdas_FunctionalInterface/StudentExample/ConsumerDemo.java
+++ Java_8_Features/src/Lambdas_FunctionalInterface/StudentExample/ConsumerDemo.java
@@ -6,21 +6,28 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
-//    static Consumer<Student> c1 = (s) -> System.out.print(s.getName() + " ");
 
+    // Before Java-8
     static Consumer<Student> c1 = new Consumer<Student>() {
         @Override
         public void accept(Student s) {
             System.out.print(s.getName() + " ");
         }
     };
+    // Using java 8 feature
+    //    static Consumer<Student> c1 = (s) -> System.out.print(s.getName() + " ");
     static Consumer<Student> c2 = (s) -> System.out.println(s.getActivities());
 
     static Consumer<Student> c3 = (s) -> System.out.print(s.getGradeLevel() + " ");
     static List<Student> studlist = StudentDatabase.getAllStudents();
 
     static void printName() {
-        studlist.forEach(c1);
+        // Before Java-8
+        for (Student s : studlist) {
+            c1.accept(s);
+        }
+        // Using java 8 feature
+//        studlist.forEach(c1);
     }
 
     static void printActivities() {
@@ -36,7 +43,8 @@ public class ConsumerDemo {
 
 
     public static void main(String[] args) {
-//        printName();
+        printName();
+        System.out.println();
         printActivities();
         System.out.println("Activities with Grade 3");
         printActivitiesWithGrade3();
