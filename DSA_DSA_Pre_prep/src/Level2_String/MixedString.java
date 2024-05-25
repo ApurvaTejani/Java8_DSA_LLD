f3e74a0e329fce6b09c39b91ca48edca8b6d7ae9
ApurvaTejani <aputejacc@gmail.com>
2024-05-25 21:55:19 +0530

    Solved MixedString Problem statment code logic issue

--- DSA_DSA_Pre_prep/src/Level2_String/MixedString.java
+++ DSA_DSA_Pre_prep/src/Level2_String/MixedString.java
@@ -5,64 +5,48 @@ package Level2_String;
 // mixString("Hi", "There") → "HTihere"
 // mixString("xxxx", "There") → "xTxhxexre"
 
-// hAving issue with the code logic
+import java.util.ArrayList;
+
+// Having issue with the code logic
 public class MixedString {
     public static void main(String[] args) {
-        System.out.println(mixString("Hi", "There"));
+
+        System.out.println(mixString("xxxx", "There"));
     }
 
     public static String mixString(String str1, String str2) {
         int l1 = str1.length();
         int l2 = str2.length();
-        char[] output = new char[l1 + l2];
-        char letters1[] = str1.toCharArray();// xxxx
-        char letters2[] = str2.toCharArray();// th
-        int j = 0;
-        int k = 0;
+        ArrayList<Character> output = new ArrayList<>();
         if (l1 == l2) {
-            for (int i = 0; i < (l1 + l2); i++) {
-
-                output[i] = letters1[j];
-                j++;
-                i++;
-                output[i] = letters2[k];
-                k++;
-
+            for (int i = 0; i < l1; i++) {
+                output.add(str1.charAt(i));
+                output.add(str2.charAt(i));
             }
         } else if (l1 > l2) {
-            j = 0;
-            k = 0;
-            int i = 0;
-            for (i = 0; i < l1; i++) {
-                output[i] = letters1[j];
-                j++;
-                i++;
-                output[i] = letters2[k];
-                k++;
-
+            for (int i = 0; i < l2; i++) {
+                output.add(str1.charAt(i));
+                output.add(str2.charAt(i));
             }
-            for (int z = i; z < l1 + l2; z++) {
-                output[z] = letters1[j];
+            for (int i = l2; i < l1; i++) {
+                output.add(str1.charAt(i));
             }
-        }
-
-        else if (l2 > l1) {
-            j = 0;
-            k = 0;
-            int i = 0;
-            for (i = 0; i < l2; i++) {
-                output[i] = letters1[j];
-                j++;
-                i++;
-                output[i] = letters2[k];
-                k++;
-
+        } else if ((l2 > l1)) {
+            for (int i = 0; i < l1; i++) {
+                output.add(str1.charAt(i));
+                output.add(str2.charAt(i));
             }
-            for (int z = i; z < l1 + l2; z++) {
-                output[z] = letters2[j];
+            for (int i = l1; i < l2; i++) {
+                output.add(str2.charAt(i));
             }
         }
-        String finalStr = new String(output);
-        return finalStr;
+        StringBuilder sb = new StringBuilder();
+        for (Character c :
+                output) {
+            sb.append(c);
+        }
+        String str = new String(sb);
+        return str;
+
     }
 }
