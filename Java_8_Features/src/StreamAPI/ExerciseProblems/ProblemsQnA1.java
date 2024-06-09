e80cba1ba2187471046b06c3634584184d2cc600
ApurvaTejani <aputejacc@gmail.com>
2024-06-09 22:38:58 +0530

    Renamed the java file

--- Java_8_Features/src/StreamAPI/ExerciseProblems/ProblemsQnA1.java
+++ Java_8_Features/src/StreamAPI/ExerciseProblems/ProblemsQnA1.java
@@ -3,20 +3,16 @@ package StreamAPI.ExerciseProblems;
 import Lambdas_FunctionalInterface.Student.Student;
 import Lambdas_FunctionalInterface.Student.StudentDatabase;
 
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.averagingInt;
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
 //Q1 Find the average GPA of female students who are in grade level 3 or higher.
 //Q2  Retrieve a sorted list of names of all students who are involved in more than two activities.
-//   Group students by gender and find the student with the highest GPA in each group.
-// Create a comma-separated string of the names of students who have read at least 5 books and have a GPA of 3.0 or higher.
-public class ProblemsQnA {
+// Q3  Group students by gender and find the student with the highest GPA in each group.
+//Q4 Create a comma-separated string of the names of students who have read at least 26 books and have a GPA of 3.0 or higher.
+public class ProblemsQnA1 {
     static List<Student> studList = StudentDatabase.getAllStudents();
 
     public static void main(String[] args) {
@@ -46,6 +42,24 @@ public class ProblemsQnA {
         System.out.println("Q2  Retrieve a sorted list of names of all students who are involved in more than two activities.");
         System.out.println(filteredList1);
 
+        //Solution to Q3
 
+        Map<String, Optional<Student>> highestGPA =
+                studList.stream()
+                        .collect(groupingBy(Student::getGender,
+                                maxBy(Comparator.comparing(Student::getGpa))));
+        System.out.println("Q3  Group students by gender and find the student with the highest GPA in each group.");
+        System.out.println(highestGPA);
+
+        //Solution to Q4
+
+        String str =
+                studList.stream()
+                        .filter(s -> s.getNoOfBooks() >= 26)
+                        .filter(s -> s.getGpa() >= 3.9)
+                        .map(Student::getName)
+                        .collect(joining(","));
+        System.out.println("Q4 Create a comma-separated string of the names of students who have read at least 26 books and have a GPA of 3.0 or higher.");
+        System.out.println(str);
     }
 }
