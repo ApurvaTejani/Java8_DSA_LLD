d452c34a6f9b7efe6c5df2d58f64377b7a79bb40
ApurvaTejani <aputejacc@gmail.com>
2024-06-09 14:42:51 +0530

    Three Params for groupingBy

---
+++ Java_8_Features/src/TerminalOperations/GroupingByExample_3.java
@@ -0,0 +1,27 @@
+package TerminalOperations;
+
+import Lambdas_FunctionalInterface.Student.Student;
+import Lambdas_FunctionalInterface.Student.StudentDatabase;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
+
+public class GroupingByExample_3 {
+    static List<Student> studList = StudentDatabase.getAllStudents();
+
+    public static void main(String[] args) {
+        //Three Param Example
+        // First Param as an Input-Key
+        //Second Param as a Output Supplier
+        // Third one as the value of that key
+        LinkedHashMap<String, Set<Student>> threeParamMap =
+                studList.stream()
+                        .collect(groupingBy(Student::getName, LinkedHashMap::new, toSet()));
+        System.out.println(threeParamMap);
+
+    }
+}
