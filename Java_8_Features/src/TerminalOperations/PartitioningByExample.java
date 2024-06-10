8600ea5839a2ffc1a75c1ad7f1da5c77d9f54ea6
ApurvaTejani <aputejacc@gmail.com>
2024-06-10 08:13:15 +0530

    Concept of PartitioningBy

---
+++ Java_8_Features/src/TerminalOperations/PartitioningByExample.java
@@ -0,0 +1,20 @@
+package TerminalOperations;
+
+import Lambdas_FunctionalInterface.Student.Student;
+import Lambdas_FunctionalInterface.Student.StudentDatabase;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.partitioningBy;
+
+public class PartitioningByExample {
+    static List<Student> studList = StudentDatabase.getAllStudents();
+
+    public static void main(String[] args) {
+        Map<Boolean, List<Student>> filteredStud = studList.stream()
+                .collect(partitioningBy(s -> s.getGpa() >= 3.8));
+        System.out.println(filteredStud);
+    }
+
+}
