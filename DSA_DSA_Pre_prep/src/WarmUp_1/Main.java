6b367d4662db9a2a0d89d623f859173f1d4fb63a
ApurvaTejani <aputejacc@gmail.com>
2024-08-11 12:56:54 +0530

    This keyword concept

---
+++ DSA_DSA_Pre_prep/src/WarmUp_1/Main.java
@@ -0,0 +1,24 @@
+package WarmUp_1;
+
+class Main {
+
+    public Main() {
+        this(2, 3);
+    }
+
+    public Main(int a, int b) {
+        System.out.println("Frist Value " + a);
+        System.out.println("Second Value " + b);
+    }
+
+    public Main print() {
+        return this;
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.print().print();
+    }
+
+}
