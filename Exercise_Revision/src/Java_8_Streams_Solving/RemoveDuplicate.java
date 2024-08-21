8a5ab1f9601f26fc6ddd70ca9433ec7c897f0f85
ApurvaTejani <aputejacc@gmail.com>
2024-08-21 10:00:00 +0530

    Updated

---
+++ Exercise_Revision/src/Java_8_Streams_Solving/RemoveDuplicate.java
@@ -0,0 +1,12 @@
+package Java_8_Streams_Solving;
+//Remove duplicate elements from a list using Java 8 streams
+//
+//        Write a Java 8 program to remove duplicate elements from a list using the stream API and lambda expressions.
+
+
+public class RemoveDuplicate {
+    
+    public static void main(String[] args) {
+
+    }
+}
